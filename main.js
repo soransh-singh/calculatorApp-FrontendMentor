1f8d34586901c32f711dbd662a55c59de5072a3b
soransh <57911107+soransh-singh@users.noreply.github.com>
2021-06-10 11:06:54 +0530

    updated main.js
    calculator can now do calculation

--- main.js
+++ main.js
@@ -2,17 +2,76 @@ window.addEventListener("load", ()=>{
   console.log("window loaded");
 
   const output = document.querySelector(".output");
-  console.log(one.dataset.value);
+
   const numBtns = document.querySelectorAll(".num-btn");
+  const calcBtns = document.querySelectorAll(".calc-btn");
+
+  const equal = document.querySelector(".equal")
+  const reset = document.querySelector(".reset")
+  const del = document.querySelector(".del")
 
   let calc = ""
+  let num1 = ""
+  let num2 = ""
+  let lastEntry = ""
 
   numBtns.forEach((numBtn) => {
     numBtn.addEventListener("click", ()=>{
-      console.log(numBtn.dataset.value);
-      output.innerHTML += numBtn.dataset.value
+      if(calc ===""){
+        num1 += numBtn.dataset.value
+        lastEntry = "num1"
+      }else{
+        num2 += numBtn.dataset.value
+        lastEntry = "num2"
+      }
+      output.innerHTML = num1 + calc + num2
     })
   });
 
+  calcBtns.forEach((calcBtn) => {
+    calcBtn.addEventListener("click", ()=>{
+      if(num1 === ""){
+
+      }else{
+        calc = calcBtn.dataset.value
+        lastEntry = "calc"
+      }
+      output.innerHTML = num1 + calc + num2
+    })
+  });
+
+  del.addEventListener("click", ()=>{
+    if(lastEntry == "num1"){
+      num1 = num1.slice(0, -1)
+    } else if(lastEntry == "num2"){
+      num2 = num2 .slice(0, -1)
+    } else if(lastEntry == "calc"){
+      calc = calc.slice(0, -1)
+    }
+    output.innerHTML = num1 + calc + num2
+  })
+
+  reset.addEventListener("click", ()=>{
+    calc = ""
+    num1 = ""
+    num2 = ""
+    output.innerHTML = num1 + calc + num2
+  })
+
+  equal.addEventListener("click", ()=>{
+    if(calc == "+"){
+      num1 = String(Number(num1) + Number(num2))
+    }else if(calc == "-"){
+      num1 = String(Number(num1) - Number(num2))
+    }else if(calc == "×"){
+      num1 = String(Number(num1) * Number(num2))
+    }else if(calc == "/"){
+      num1 = String(Number(num1) / Number(num2))
+    }
+    calc = ""
+    num2 = ""
+    lastEntry = "num1"
+    output.innerHTML = num1 + calc + num2
+  })
 
 })
