f92d7095dc14cba0c7d47c7a50517b0df4d54d9f
soransh <57911107+soransh-singh@users.noreply.github.com>
2021-06-10 23:00:23 +0530

    update theme

--- main.js
+++ main.js
@@ -15,6 +15,7 @@ window.addEventListener("load", ()=>{
   let num2 = ""
   let lastEntry = ""
 
+  /*function to render result in output div*/
   function  result() {
     if(calc == "+"){
       num1 = String(Number(num1) + Number(num2))
@@ -31,7 +32,107 @@ window.addEventListener("load", ()=>{
     output.innerHTML = num1 + calc + num2
   }
 
+  /*Theme changing section*/
+  const themeSwitches = document.querySelectorAll(".theme-switch")
+  const toggleSwitch = document.querySelector(".toggle-switch")
+  const toggle = document.querySelector(".toggle")
 
+  const toggleSwitchClass =   toggleSwitch.classList
+
+  const root = document.documentElement.style
+  function first(){
+    root.setProperty("--bg-main", "hsl(222, 26%, 31%)")
+    root.setProperty("--bg-toggle", "hsl(223, 31%, 20%)")
+    root.setProperty("--bg-screen", "hsl(224, 36%, 15%)")
+
+    root.setProperty("--key-del-bg", "hsl(225, 21%, 49%)")
+    root.setProperty("--key-del-shadow", "hsl(224, 28%, 35%)")
+
+    root.setProperty("--key-equal-bg", "hsl(6, 63%, 50%)")
+    root.setProperty("--key-equal-shadow", "hsl(6, 70%, 34%)")
+
+    root.setProperty("--key-bg", "hsl(30, 25%, 89%)")
+    root.setProperty("--key-shadow", "hsl(28, 16%, 65%)")
+
+    root.setProperty("--text-pr", "hsl(221, 14%, 31%)")
+    root.setProperty("--text-sec", "hsl(0, 0%, 100%)")
+    root.setProperty("--text-white", "hsl(0, 0%, 100%)")
+
+    root.setProperty("--text-ter", "hsl(0, 0%, 100%)")
+    toggleSwitchClass.remove("toggle-half")
+    toggleSwitchClass.remove("toggle-full")
+
+
+  }
+
+  function second(){
+    root.setProperty("--bg-main", "hsl(0, 0%, 90%)")
+    root.setProperty("--bg-toggle", "hsl(0, 5%, 81%)")
+    root.setProperty("--bg-screen", "hsl(0, 0%, 93%)")
+
+    root.setProperty("--key-del-bg", "hsl(185, 42%, 37%)")
+    root.setProperty("--key-del-shadow", "hsl(185, 58%, 25%)")
+
+    root.setProperty("--key-equal-bg", "hsl(25, 98%, 40%)")
+    root.setProperty("--key-equal-shadow", "hsl(25, 99%, 27%)")
+
+    root.setProperty("--key-bg", "hsl(45, 7%, 89%)")
+    root.setProperty("--key-shadow", "hsl(35, 11%, 61%)")
+
+    root.setProperty("--text-pr", "hsl(60, 10%, 19%)")
+    root.setProperty("--text-sec", "hsl(0, 0%, 100%)")
+    root.setProperty("--text-white", "hsl(0, 0%, 100%)")
+
+    root.setProperty("--text-ter", "hsl(60, 10%, 19%)")
+
+    toggleSwitchClass.add("toggle-half")
+    toggleSwitchClass.remove("toggle-full")
+  }
+
+  function third(){
+    root.setProperty("--bg-main", "hsl(268, 75%, 9%)")
+    root.setProperty("--bg-toggle", "hsl(268, 71%, 12%)")
+    root.setProperty("--bg-screen", "hsl(268, 71%, 12%)")
+
+    root.setProperty("--key-del-bg", "hsl(281, 89%, 26%)")
+    root.setProperty("--key-del-shadow", "hsl(285, 91%, 52%)")
+
+    root.setProperty("--key-equal-bg", "hsl(176, 100%, 44%)")
+    root.setProperty("--key-equal-shadow", "hsl(177, 92%, 70%)")
+
+    root.setProperty("--key-bg", "hsl(268, 47%, 21%)")
+    root.setProperty("--key-shadow", "hsl(290, 70%, 36%)")
+
+    root.setProperty("--text-pr", "hsl(52, 100%, 62%)")
+    root.setProperty("--text-sec", "hsl(198, 20%, 13%)")
+    root.setProperty("--text-white", "hsl(0, 0%, 100%)")
+
+    root.setProperty("--text-ter", "hsl(52, 100%, 62%)")
+
+    toggleSwitchClass.remove("toggle-half")
+    toggleSwitchClass.add("toggle-full")
+  }
+
+
+  themeSwitches.forEach((theme) => {
+    theme.addEventListener("click", ()=>{
+      if(theme.dataset.theme === "first"){
+        first()
+      }else if(theme.dataset.theme === "second"){
+        second()
+      } else if(theme.dataset.theme === "third"){
+        third()
+      }
+    })
+  })
+
+
+  toggle.addEventListener("click", ()=>{
+    console.log("hello");
+  })
+
+
+  /* event listener section Calculation Logic */
   numBtns.forEach((numBtn) => {
     numBtn.addEventListener("click", ()=>{
       if(calc ===""){
