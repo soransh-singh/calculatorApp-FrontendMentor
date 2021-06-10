b8185c56ca5fe23079c8aeb242df8c69bbb9546b
soransh <57911107+soransh-singh@users.noreply.github.com>
2021-06-10 09:43:59 +0530

    update css and js

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 window.addEventListener("load", ()=>{
   console.log("window loaded");
+
+  const output = document.querySelector(".output");
+  console.log(one.dataset.value);
+  const numBtns = document.querySelectorAll(".num-btn");
+
+  let calc = ""
+
+  numBtns.forEach((numBtn) => {
+    numBtn.addEventListener("click", ()=>{
+      console.log(numBtn.dataset.value);
+      output.innerHTML += numBtn.dataset.value
+    })
+  });
+
+
 })
